64ba1e73f47da8ecf52dab18e7ed2820a86c13e4
seasun93 <seasun9376@gmail.com>
2023-11-21 02:14:27 +0900

    feat:part3 메뉴 효과 작업

--- js/index.js
+++ js/index.js
@@ -1,3 +1,47 @@
-window.addEventListener('ready',function(){
-  //메뉴에 마우스가 올라오면 nav-list-wrap이 on되고 빠져나가면 안보이기
+window.addEventListener('load',()=>{
+  const topMenu = document.querySelectorAll('nav > ul > li');
+  const navBg = document.querySelector('.nav-bg');
+
+  listTwoTitle();
+
+  topMenu.forEach((el,i)=>{
+    el.addEventListener('mouseover',()=>{
+      const target = el.getElementsByClassName('nav-list-deps1');
+      listReset();
+      navBg.classList.add('on')
+      target[0].classList.add('on');
+    })
+    el.addEventListener('mouseleave',()=>{
+      listReset();
+      navBg.classList.remove('on')
+    })
+  })
+
 })
+
+function listReset(){
+  const topMenu = document.querySelectorAll('nav > ul > li');
+  for(let i = 0; i< topMenu.length; i++){
+    topMenu[i].lastElementChild.classList.remove('on')
+  }
+}
+
+function listTwoTitle(){
+  //li안에 자식이 2개 이상이면 3번째 자식에게 class명 추가
+  const topMenu = document.querySelectorAll('nav > ul > li');
+  topMenu.forEach((el,i)=>{
+    const midMenu = el.children[1].children
+    for(let j=0; j<midMenu.length; j++){
+      console.log(midMenu[j].children)
+      if(midMenu[j].children.length > 2){
+        console.log((midMenu[j].children[2]))
+        midMenu[j].children[2].classList.add('divid-title')
+      }
+    }
+  })
+  for(let i = 0; i<topMenu.length; i++){
+    if(topMenu[0].children.length > 2){
+
+    }
+  }
+}
